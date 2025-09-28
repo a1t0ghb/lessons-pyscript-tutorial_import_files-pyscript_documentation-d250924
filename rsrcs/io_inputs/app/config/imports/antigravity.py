ebcf4e351dcda3bf5226cd00963ada96e4b0117c
a1t0git <github_ctc101@a1t0.aleeas.com>
2025-09-28 10:05:31 -0500

    refactor: re-organize content from one 'index.html' to multiple pages, starting with 'home.html'.

--- rsrcs/io_inputs/app/config/imports/antigravity.py
+++ rsrcs/io_inputs/app/config/imports/antigravity.py
@@ -7,7 +7,7 @@ from pyodide.ffi.wrappers import set_interval
 
 class Antigravity:
 
-    url = "rsrcs/media/img-antigravity-white.svg"        #  Path with respect to 'HOME'.
+    url = "./../../../../media/img-antigravity-white.svg"           #  Source image. Goes up x4 folder levels to get to 'media' (rsrcs < io_inputs < app < config < web_pages), where it's stored.
 
     def __init__(self, target=None, interval=10, append=True, fly=False):
         if isinstance(target, str):
